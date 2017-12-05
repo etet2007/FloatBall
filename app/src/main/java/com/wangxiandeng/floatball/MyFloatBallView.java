34e3283eba43c2ea325bfe2fda1292bbccda4230
Stephenlau <120220149@qq.com>
2017-12-05 17:37:53 +0800

    u

--- app/src/main/java/com/wangxiandeng/floatball/MyFloatBallView.java
+++ app/src/main/java/com/wangxiandeng/floatball/MyFloatBallView.java
@@ -11,10 +11,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v4.view.GestureDetectorCompat;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by stephenlau on 2017/12/5.
@@ -26,13 +29,12 @@ public class MyFloatBallView extends View {
     private Paint ballPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private float backgroundRadius=40;
 
-    public float getBallRadius() {
-        return ballRadius;
-    }
+    private boolean isLongPress=false;
+    private int mOffsetToParent;
+    private int mOffsetToParentY;
+    private WindowManager.LayoutParams mLayoutParams;
+    private int mStatusBarHeight;
 
-    public void setBallRadius(float ballRadius) {
-        this.ballRadius = ballRadius;
-    }
 
     private float ballRadius=25;
 
@@ -41,6 +43,21 @@ public class MyFloatBallView extends View {
 
     private WindowManager mWindowManager;
     ObjectAnimator clickAnim;
+
+
+    public void setLayoutParams(WindowManager.LayoutParams params) {
+        mLayoutParams = params;
+    }
+
+    public float getBallRadius() {
+        return ballRadius;
+    }
+
+    public void setBallRadius(float ballRadius) {
+        this.ballRadius = ballRadius;
+    }
+
+
     public MyFloatBallView(Context context) {
         super(context);
         mService = (AccessibilityService) context;
@@ -56,8 +73,8 @@ public class MyFloatBallView extends View {
 
 
         Keyframe kf0 = Keyframe.ofFloat(0f, 25);
-        Keyframe kf1 = Keyframe.ofFloat(.4f, 35);
-        Keyframe kf2 = Keyframe.ofFloat(.6f, 35);
+        Keyframe kf1 = Keyframe.ofFloat(.4f, 32);
+        Keyframe kf2 = Keyframe.ofFloat(.8f, 35);
         Keyframe kf3 = Keyframe.ofFloat(1f, 25);
         PropertyValuesHolder click = PropertyValuesHolder.ofKeyframe("ballRadius", kf0, kf1,kf2,kf3);
         clickAnim = ObjectAnimator.ofPropertyValuesHolder(this, click);
@@ -69,6 +86,10 @@ public class MyFloatBallView extends View {
             }
         });
 
+
+        mStatusBarHeight = getStatusBarHeight();
+        mOffsetToParent = dip2px(backgroundRadius/2);
+        mOffsetToParentY = mStatusBarHeight + mOffsetToParent;
     }
 
     @Override
@@ -78,7 +99,6 @@ public class MyFloatBallView extends View {
         canvas.drawCircle(50, 50, backgroundRadius, backgroundPaint);
         canvas.drawCircle(50, 50, ballRadius, ballPaint);
 
-//        Log.d("lqt", "onDraw: ");
     }
 
     @Override
@@ -91,15 +111,19 @@ public class MyFloatBallView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         this.mDetector.onTouchEvent(event);
 
-//        switch (event.getAction()) {
-//            case MotionEvent.ACTION_DOWN:
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                break;
-//            case MotionEvent.ACTION_CANCEL:
-//            case MotionEvent.ACTION_UP:
-//                break;
-//        }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                if (isLongPress){
+                    mLayoutParams.x = (int) (event.getRawX() - mOffsetToParent);
+                    mLayoutParams.y = (int) (event.getRawY() - mOffsetToParentY);
+                    mWindowManager.updateViewLayout(MyFloatBallView.this, mLayoutParams);
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                isLongPress=false;
+                break;
+        }
         return true;
     }
 
@@ -133,11 +157,35 @@ public class MyFloatBallView extends View {
         @Override
         public void onLongPress(MotionEvent e) {
             Log.d(TAG, "onLongPress: ");
+            isLongPress=true;
         }
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
             return false;
         }
+    }
+    /**
+     * 获取通知栏高度
+     *
+     * @return
+     */
+    private int getStatusBarHeight() {
+        int statusBarHeight = 0;
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object o = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = (Integer) field.get(o);
+            statusBarHeight = getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusBarHeight;
+    }
+    public int dip2px(float dip) {
+        return (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dip, getContext().getResources().getDisplayMetrics()
+        );
     }
 }
